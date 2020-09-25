cce7db5c75e8eaac7839020cebee32bf4610358c
Sonu Aryan <57495903+cosmo-developer@users.noreply.github.com>
2020-09-25 07:01:10 +0530

    changes done

--- mac/setup.py
+++ mac/setup.py
@@ -1,12 +1,6 @@
 from setuptools import Extension,setup
 from Cython.Build import cythonize
 import numpy as np
-import os
-print("If you don't have lapack and blas then need to install lapack and blas")
-print("If not builded failed then use command to install some stuff")
-print("To install lapack use command 'brew install lapack'")
-print("To install openblas use command 'brew install openblas'")
-os.system("apt-get install libblas-dev liblapack-dev")
 setup(
     ext_modules=cythonize(Extension(
 	  name="midasml2",
@@ -18,7 +12,7 @@ setup(
               'src/ld_estim.cpp'
               ],
           language='c++',
-          libraries=['openblas','lapack']
+          libraries=['blas','lapack']
 	  )
 	),
 	language='c++',
